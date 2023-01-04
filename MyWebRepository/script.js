8edff7b38d8b884aec7d875177a090a239455031
Gyan Prakash Yadav <104619163+gyan0077@users.noreply.github.com>
2023-01-04 20:06:07 +0530

    Add files via upload

---
+++ MyWebRepository/script.js
@@ -0,0 +1,36 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
+function bigImg(x) {
+  x.style.height = "44px";
+  x.style.width = "44px";
+}
+
+function normalImg(x) {
+  x.style.height = "22px";
+  x.style.width = "22px";
+}
